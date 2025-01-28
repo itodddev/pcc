df2324f580866f7620584304bd593cdb824f26fa
Todd James <todd@itodd.co>
2025-01-27 20:30:58 -0500

    Chapter 3

---
+++ Chapter3/greetings_3_2.py
@@ -0,0 +1,6 @@
+friends = ['Kelli', 'Ella', 'Ruby', 'Pat']
+greeting = "Hello,"
+print(f'{greeting} {friends[0]}')
+print(f'{greeting} {friends[1]}')
+print(f'{greeting} {friends[2]}')
+print(f'{greeting} {friends[3]}')
---
+++ Chapter3/names_3_1.py
@@ -0,0 +1,5 @@
+friends = ['Kelli', 'Ella', 'Ruby', 'Pat']
+print(friends[0])
+print(friends[1])
+print(friends[-2])
+print(friends[-1])
